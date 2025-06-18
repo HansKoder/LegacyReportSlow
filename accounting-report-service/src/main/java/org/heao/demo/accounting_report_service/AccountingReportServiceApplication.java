d61a3f765afd26204d9158bb3cd480c5e3a4156e
hans.arias <hansarias74@gmail.com>
2025-06-18 13:48:28 -0500

    Set up spring boot version 3 without code

---
+++ accounting-report-service/src/main/java/org/heao/demo/accounting_report_service/AccountingReportServiceApplication.java
@@ -0,0 +1,13 @@
+package org.heao.demo.accounting_report_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingReportServiceApplication.class, args);
+	}
+
+}
---
+++ accounting-report-service/src/test/java/org/heao/demo/accounting_report_service/AccountingReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.heao.demo.accounting_report_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
